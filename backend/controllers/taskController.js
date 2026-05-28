44e24612a4b5b7126dee676d2e5e4dae1e0cc6a1
Meghana-Merla <24je0654@iitism.ac.in>
2026-05-28 22:22:29 +0530

    Implement Full CRUD APIs for Tasks

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -26,6 +26,78 @@ const createTask = async (req, res) => {
 
 }
 
+const getTasks = async (req, res) => {
+
+  try {
+
+    const tasks = await Task.find()
+
+    res.status(200).json(tasks)
+
+  }
+
+  catch (error) {
+
+    res.status(500).json({
+      message: error.message
+    })
+
+  }
+
+}
+const updateTask = async (req, res) => {
+
+  try {
+
+    const updatedTask = await Task.findByIdAndUpdate(
+
+      req.params.id,
+
+      req.body,
+
+      { new: true }
+
+    )
+
+    res.status(200).json(updatedTask)
+
+  }
+
+  catch (error) {
+
+    res.status(500).json({
+      message: error.message
+    })
+
+  }
+
+}
+
+const deleteTask = async (req, res) => {
+
+  try {
+
+    await Task.findByIdAndDelete(req.params.id)
+
+    res.status(200).json({
+      message: "Task deleted successfully"
+    })
+
+  }
+
+  catch (error) {
+
+    res.status(500).json({
+      message: error.message
+    })
+
+  }
+
+}
+
 module.exports = {
-  createTask
+  createTask,
+  getTasks,
+  updateTask,
+  deleteTask
 }
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -2,7 +2,24 @@ const express = require("express")
 
 const router = express.Router()
 
-const { createTask } = require("../controllers/taskController")
+const {
+  createTask,
+  getTasks,
+    updateTask,
+    deleteTask
+} = require("../controllers/taskController")
+
+/**
+ * @swagger
+ * /api/tasks:
+ *   get:
+ *     summary: Get all tasks
+ *     responses:
+ *       200:
+ *         description: Successfully fetched tasks
+ */
+
+router.get("/tasks", getTasks)
 
 /**
  * @swagger
@@ -25,5 +42,51 @@ const { createTask } = require("../controllers/taskController")
  */
 
 router.post("/tasks", createTask)
+
+/**
+ * @swagger
+ * /api/tasks/{id}:
+ *   put:
+ *     summary: Update a task
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               completed:
+ *                 type: boolean
+ *                 example: true
+ *     responses:
+ *       200:
+ *         description: Task updated successfully
+ */
+
+router.put("/tasks/:id", updateTask)
+
+/**
+ * @swagger
+ * /api/tasks/{id}:
+ *   delete:
+ *     summary: Delete a task
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Task deleted successfully
+ */
+
+router.delete("/tasks/:id", deleteTask)
 
 module.exports = router
